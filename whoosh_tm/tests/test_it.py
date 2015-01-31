be665af1cfda0fa37615045f07c1eb9fb0c12bb5
Atsushi Odagiri <aodagx@gmail.com>
2015-01-31 16:49:37 +0900

    added tests for aborting

--- whoosh_tm/tests/test_it.py
+++ whoosh_tm/tests/test_it.py
@@ -35,3 +35,28 @@ class TestIt(unittest.TestCase):
                 self.assertEqual(len(results), 1)
 
                 self.assertEqual(results[0]["title"], "Second document")
+
+    def test_abort(self):
+        import transaction
+        from whoosh_tm.datamanager import WhooshDataManager
+        from whoosh.qparser import QueryParser
+        from whoosh.index import create_in, open_dir
+        with TempDirectory() as d:
+            ix = create_in(d.path, dummy_schema)
+            dm = WhooshDataManager(ix)
+            t = transaction.get()
+
+            dm.add_document(
+                title=u"First document", path=u"/a",
+                content=u"This is the first document we've added!")
+            dm.add_document(
+                title=u"Second document", path=u"/b",
+                content=u"The second one is even more interesting!")
+            t.join(dm)
+            transaction.abort()
+
+            ix2 = open_dir(d.path)
+            with ix2.searcher() as searcher:
+                parser = QueryParser("content", ix2.schema)
+                results = searcher.search(parser.parse('second'))
+                self.assertEqual(len(results), 0)
